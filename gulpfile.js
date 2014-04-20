ac29c6e2f3d85bcdc2ab19f8a0f0e8a71bc28836
Kevin Suttle <kevin@suttle.io>
2014-04-20 15:02:37 -0500

    Add gulpfile and package.json
    Thanks again, @shakyShane!

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (cb) {
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', function () {
+            cb();
+        });
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Compile files from _sass into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    gulp.src('_sass/index.scss')
+        .pipe(sass({includePaths: ['_sass']}))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch liquid/md/ files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_sass/*.scss', ['sass']);
+    gulp.watch(['_layouts/*.liquid', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
